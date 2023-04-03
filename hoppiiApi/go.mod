e1b972f4374854d87d01edfd1555444bdb3a795f
takaryo1010 <t.ryota1010@gmail.com>
2023-04-03 13:44:04 +0900

    start commit

---
+++ hoppiiApi/go.mod
@@ -0,0 +1,12 @@
+module main
+
+go 1.19
+
+require github.com/isso-719/hoppii-api v0.0.1
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
---
+++ hoppiiApi/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/isso-719/hoppii-api"
+	"github.com/isso-719/hoppii-api/pkg/announcement"
+	"github.com/isso-719/hoppii-api/pkg/assignment"
+	"github.com/isso-719/hoppii-api/pkg/user"
+)
+
+func main() {
+	// ここに学籍番号を入力
+	studentId := "99z9999"
+	// ここにパスワードを入力
+	password := "password"
+
+	hoppiiApi := hoppii_api.NewHoppiiApi()
+
+	// ユーザクライアントの作成 (これを使用して Hoppii にアクセスする)
+	user, err := hoppiiApi.User.CreateUser(&user.UserInput{
+		StudentId: studentId,
+		Password:  password,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Announcement Entity の User Action を取得
+	auo, err := hoppiiApi.Announcement.User(&announcement.AnnouncementUserInput{
+		UserCookie: user.Cookie,
+	})
+	if err != nil {
+		panic(err)
+	}
+	// 結果を出力
+	fmt.Println(auo.AnnouncementUserResult.AnnouncementCollection[0].Title)
+
+	// Assignment Entity の My Action を取得
+	ao, err := hoppiiApi.Assignment.My(&assignment.AssignmentMyInput{
+		UserCookie: user.Cookie,
+	})
+	if err != nil {
+		panic(err)
+	}
+	// 結果を出力
+	fmt.Println(ao.AssignmentMyResult.AssignmentCollection[0].Title)
+}
